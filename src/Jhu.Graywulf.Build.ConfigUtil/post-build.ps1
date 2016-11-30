4f0427571080c5e80a9cd3a4ca46c377e6ebc474
Laszlo Dobos <dobos@complex.elte.hu>
2016-11-30 15:33:32 -0500

    Modified post build not to cause full solution rebuilds.

--- src/Jhu.Graywulf.Build.ConfigUtil/post-build.ps1
+++ src/Jhu.Graywulf.Build.ConfigUtil/post-build.ps1
@@ -11,4 +11,5 @@ cp "${ProjectDir}${OutDir}$TargetName.exe" "${SolutionDir}${OutDir}"
 cp "${ProjectDir}${OutDir}$TargetName.exe.config" "${SolutionDir}${OutDir}"
 cp "${ProjectDir}${OutDir}$TargetName.pdb" "${SolutionDir}${OutDir}"
 
-& "${SolutionDir}${OutDir}gwconfig.exe" merge $SolutionDir$SolutionName.sln $ProjectName
+# No need to reconfigure itself
+# & "${SolutionDir}${OutDir}gwconfig.exe" merge $SolutionDir$SolutionName.sln $ProjectName
