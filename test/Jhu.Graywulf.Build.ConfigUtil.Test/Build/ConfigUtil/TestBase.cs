0327983ff9548bc155c9a640769e382b83ba517e
Laszlo Dobos <dobos@complex.elte.hu>
2016-12-06 19:48:49 -0500

    Unit tests fixed.

--- test/Jhu.Graywulf.Build.ConfigUtil.Test/Build/ConfigUtil/TestBase.cs
+++ test/Jhu.Graywulf.Build.ConfigUtil.Test/Build/ConfigUtil/TestBase.cs
@@ -5,28 +5,44 @@ namespace Jhu.Graywulf.Build.ConfigUtil
 {
     public abstract class TestBase
     {
+        private static string GetSolutionPath()
+        {
+            var sln = Path.GetDirectoryName(Environment.GetEnvironmentVariable("SolutionPath"));
+            return sln;
+        }
+
+        private string MapPath(string path)
+        {
+            string file;
+
+            file = Path.Combine(Environment.CurrentDirectory, @"..\..\..\..", path);
+
+            if (!File.Exists(file) && !Directory.Exists(file))
+            {
+                file = Path.Combine(GetSolutionPath(), path);
+            }
+
+            if (!File.Exists(file) && !Directory.Exists(file))
+            {
+                file = Path.Combine(GetSolutionPath(), "graywulf-build", path);
+            }
+
+            return file;
+        }
+
         protected string GetTestConfigPath()
         {
-            var path = Environment.CurrentDirectory;
-            path = Path.Combine(path, @"..\..\..\..\test\config\");
-
-            return path;
+            return MapPath(@"\test\config\");
         }
 
         protected string GetTestSolutionPath()
         {
-            var path = Environment.CurrentDirectory;
-            path = Path.Combine(path, @"..\..\..\..\graywulf-build.sln");
-
-            return path;
+            return MapPath(@"graywulf-build.sln");
         }
 
         protected string GetTestProjectPath()
         {
-            var path = Environment.CurrentDirectory;
-            path = Path.Combine(path, @"..\..\Jhu.Graywulf.Build.ConfigUtil.Test.csproj");
-
-            return path;
+            return MapPath(@"test\Jhu.Graywulf.Build.ConfigUtil.Test\Jhu.Graywulf.Build.ConfigUtil.Test.csproj");
         }
     }
 }
